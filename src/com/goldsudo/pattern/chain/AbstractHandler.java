0ebd943d81043fa05383534f1af434bd9fd12949
goldsudo <goldsudo@gmail.com>
2018-08-21 01:10:59 +0800

    责任链模式

--- src/com/goldsudo/pattern/chain/AbstractHandler.java
+++ src/com/goldsudo/pattern/chain/AbstractHandler.java
@@ -3,17 +3,13 @@ package com.goldsudo.pattern.chain;
 public abstract class AbstractHandler {
     private Handler handler;
 
-    public Handler getHandler() {
-        return handler;
-    }
-
     public void setHandler(Handler handler) {
         this.handler = handler;
     }
 
     public void next() {
-        if (getHandler() != null) {
-            getHandler().operator();
+        if (this.handler != null) {
+            this.handler.operator();
         }
     }
 }
